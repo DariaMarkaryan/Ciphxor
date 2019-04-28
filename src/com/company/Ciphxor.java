6f65bfe1ab3e44d4339383722aed03ccd57e668e
Dasha Fedorova <poooroh@mail.ru>
2019-04-28 21:09:54 +0300

    Final fixes

--- src/com/company/Ciphxor.java
+++ src/com/company/Ciphxor.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.io.*;
-import java.nio.ByteBuffer;
 
 @SuppressWarnings("WeakerAccess")
 
@@ -22,70 +21,32 @@ import java.nio.ByteBuffer;
         return res;
     }
 
-    private static String decryption(String msg, byte[] key ) {
-        String hexToPairs = "";
-        int i = 0;
-        while (i < msg.length() - 1) {
-            String output = msg.substring(i, i + 2);
-            int decimal = Integer.parseInt(output, 16);
-            hexToPairs += (char)decimal;
-            i += 2;
-        }
-        char[] hexToPairsArr = hexToPairs.toCharArray();
-        String decryptedText = "";
-        int keyItr = 0;
-        for (int j = 0; j < hexToPairsArr.length; j++) {
-            byte temp =(byte)((byte)hexToPairsArr[j] ^ key[keyItr % key.length]);
-            decryptedText += (char)temp;
-            keyItr++;
-        }
-        return decryptedText;
-    }
-
-    private static String encryption(String msg, byte[] key){
-        int Itr = 0;
-        String res = "";
-
-        for (int j = 0; j < msg.length(); j++) {
-            int temp = msg.charAt(j) ^ key[Itr % key.length];
-            res += String.format("%02x", (byte) temp);
-            Itr++;
-        }
-    return res;
-    }
-
     private static void recode(FileInputStream inputStream, String outputFileName, String ekey, String dkey)
             throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        File outFile = new File(outputFileName);
-        FileWriter fw = new FileWriter(outFile, false);
-        String encrypHexa = "";
-        byte[] ekeyarr = keyToByteArray(ekey);
-        byte[] dkeyarr = keyToByteArray(dkey);
-        String rawMsg = "";
+        FileOutputStream out = new FileOutputStream(outputFileName);
+        byte[] keyarr;
 
-        if (ekey != "") {
-            while((rawMsg = br.readLine()) != null){
-                encrypHexa = encryption(rawMsg,ekeyarr);
-                if (dkey != "") encrypHexa = decryption(encrypHexa,dkeyarr);
-                fw.append(encrypHexa);
-                fw.write("\r\n");
-            }
-        } else if (dkey != "") {
-            while((rawMsg = br.readLine()) != null) {
-                encrypHexa = decryption(rawMsg, dkeyarr);
-                fw.append(encrypHexa);
-                fw.write("\r\n");
+        if (ekey != "" && dkey != "") {
+            throw new IOException("Задайте только один ключ");
+        }
+        if (ekey != "" || dkey != "") {
+            if(ekey!="") keyarr = keyToByteArray(ekey);
+            else keyarr = keyToByteArray(dkey);
+            byte temp;
+            byte res;
+            int i = 0;
+            while ((temp = (byte)inputStream.read()) != -1) {
+                res = (byte)(temp^keyarr[i]);
+                i++;
+                i = i % keyarr.length;
+                out.write(res);
             }
         } else {
-            while((rawMsg = br.readLine()) != null){
-                fw.append(rawMsg);
-                fw.write("\r\n");
-            }
+            throw new IOException("нет ключа");
         }
-        fw.flush();
-        fw.close();
-        br.close();
+        out.flush();
+        out.close();
+        inputStream.close();
 }
 
     public static void recode(String inputFileName, String outputFileName, String ekey, String dkey)
